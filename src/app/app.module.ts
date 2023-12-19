3506333935435597a0cea65dca5c34963a44c056
aren talb <aren.talb00@gmail.com>
2023-12-19 16:24:58 +0300

    Design data section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
